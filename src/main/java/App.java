350eb7c224419cd8c3b2e70ff22b5d036656253f
IgorDerik <igorderik@yahoo.com>
2018-05-04 06:00:26 +0300

    test commit

---
+++ src/main/java/App.java
@@ -0,0 +1,37 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+    private Client client;
+    private EventLogger eventLogger;
+
+    public App(Client client, EventLogger eventLogger) {
+        this.client = client;
+        this.eventLogger = eventLogger;
+    }
+
+    public void logEvent(String msg) {
+
+        String message = msg.replaceAll(client.getId(), client.getFullName());
+
+        eventLogger.logEvent(message);
+    }
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+
+        App app = (App) ctx.getBean("app");
+
+        app.logEvent("Some event for 1");
+        app.logEvent("Some event for 2");
+
+//        App app = new App();
+//        app.client = new Client("1", "John Smith");
+//        app.consoleEventLogger = new ConsoleEventLogger();
+//        app.logEvent("Some event for user 1");
+
+    }
+
+}
---
+++ src/main/java/ConsoleEventLogger.java
@@ -0,0 +1,9 @@
+public class ConsoleEventLogger implements EventLogger {
+
+    public void logEvent(String msg) {
+
+        System.out.println(msg);
+
+    }
+
+}
