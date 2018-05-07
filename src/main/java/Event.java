80ac54644df054863072f030c6b9a95d042c1419
Igor Dereviankin <Igor_Dereviankin@epam.com>
2018-05-07 18:58:57 +0300

    Event class modified

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,5 +1,33 @@
+import java.util.Date;
+
 public class Event {
 
-    
+    private int id;
+    private String msg;
+    private Date date;
+
+    public Event(Date date) {
+        this.date = date;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", msg='" + msg + '\'' +
+                ", date=" + date +
+                '}';
+    }
+
+    /*
+    - id: int; // Auto-generated
+    - msg: String; // Set in setter
+    - date: Date; // Set in constructor
+    + toString()
+    */
 
 }
