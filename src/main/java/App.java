f2ba735b643ac07d0d0086c42f6fb3ec2e788d79
IgorDerik <igorderik@yahoo.com>
2018-05-08 02:38:52 +0300

    fixed bugs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,26 +11,25 @@ public class App {
         this.eventLogger = eventLogger;
     }
 
-    public void logEvent(String msg) {
+    public void logEvent(Event event, String msg) {
 
         String message = msg.replaceAll(client.getId(), client.getFullName());
 
-    //    eventLogger.logEvent(message);
+        event.setMsg(message);
+
+        eventLogger.logEvent(event);
     }
 
     public static void main(String[] args) {
 
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
-
         App app = (App) ctx.getBean("app");
 
-        app.logEvent("Some event for 1");
-        app.logEvent("Some event for 2");
+        Event event = ctx.getBean(Event.class);
+        app.logEvent(event, "Some event for 1");
 
-//        App app = new App();
-//        app.client = new Client("1", "John Smith");
-//        app.consoleEventLogger = new ConsoleEventLogger();
-//        app.logEvent("Some event for user 1");
+        event = ctx.getBean(Event.class);
+        app.logEvent(event,"Some event for 2");
 
     }
 
